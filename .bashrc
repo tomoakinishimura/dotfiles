d3cc43ac6c87ca207f4204820263a1376d3e934c
tomoakinishimura <n.t0226@gmail.com>
2018-06-28 09:50:17 +0900

    bashを.bashrc.dに変更。再帰的に読み込み

--- .bashrc
+++ .bashrc
@@ -5,10 +5,12 @@ if [ -f /etc/bashrc ]; then
 	. /etc/bashrc
 fi
 
-# User specific aliases and functions
-alias be='bundle exec'
-alias railss='rails server -b 0.0.0.0'
-alias rubocopdiff='rubocop $( git diff --name-only --diff-filter=AMRC | grep -e ".*\.rb" | paste -s - )'
+# Source all files in ~/.bashrc.d/
+for i in ~/.bashrc.d/* ; do
+  if [ -r "$i" ]; then
+    source $i
+  fi
+done
 
 #pecoでhistory検索
 export HISTCONTROL="ignoredups"
---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Script to install my dotfiles
+
+BACKUPDIR="$HOME/dotfiles-backup/"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )
+
+link_file ()
+{
+
+  if [ -h "$HOME/$1" ]; then
+    if [ "$(pwd -P $HOME/$1)/$1" == $DIR/$1 ]; then
+      echo "Your $1 is already linked up, good job!"
+    else
+      echo "Your $1 is already a symlink to something else, I'll leave you to fix that. Quickest fix is to delete the link and run this script again."
+    fi
+  else
+    if [ -a "$HOME/$1" ]; then
+
+      echo "Your $1 already exists, backing up to $BACKUPDIR and replacing."
+      if [ $1 == .bashrc ] || [ $1 == .bash_profile ]; then
+        echo "You might want to copy it into .bashrc.d if you want to keep it."
+      fi
+      mkdir $BACKUPDIR
+      mv $HOME/$1 $BACKUPDIR
+
+    fi
+
+    ln -s $DIR/$1 $HOME/$1
+    if [ $? -eq 0 ]; then
+      echo "$DIR/$1 successfully linked to $HOME/$1."
+    else
+      echo "$DIR/$1 failed to link to $HOME/$1. Investigate and try again."
+      exit 1
+    fi
+
+  fi
+}
+
+if [ ! -d "$HOME/Projects/forks/zsh-git-prompt" ]; then
+  git clone https://github.com/olivierverdier/zsh-git-prompt.git $HOME/Projects/forks/zsh-git-prompt
+fi
+
+link_file .bashrc
+link_file .bashrc.d
+link_file .vimrc
+
+git submodule foreach --recursive git checkout master
